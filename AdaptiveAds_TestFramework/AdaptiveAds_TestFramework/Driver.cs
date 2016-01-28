86f9135e1ba146f3bd840f45d6b271c396cc7d4f
liamharries <liamharries@googlemail.com>
2016-01-28 16:50:43 +0000

    Cleaned up and commented Driver and Route

--- AdaptiveAds_TestFramework/AdaptiveAds_TestFramework/Driver.cs
+++ AdaptiveAds_TestFramework/AdaptiveAds_TestFramework/Driver.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.ObjectModel;
-using System.Runtime.InteropServices;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 
 namespace AdaptiveAds_TestFramework
 {
+    /// <summary>
+    /// Contains functionality for browser automation.
+    /// </summary>
     public static class Driver
     {
         #region Variables
 
+        /// <summary>
+        /// Period of time.
+        /// </summary>
         public enum Period
         {
             None,
@@ -17,7 +22,6 @@ namespace AdaptiveAds_TestFramework
             Default,
             Long
         }
-
 
         private static Period _waitPeriod;
 
@@ -27,9 +31,12 @@ namespace AdaptiveAds_TestFramework
 
         #region Constructor
 
+        /// <summary>
+        /// Constructs class with list Routes
+        /// </summary>
         static Driver()
         {
-            NavigableLocations = new Collection<Route>
+            Routes = new Collection<Route>
             {
                 new Route(Location.Login, "/auth/login"),
                 new Route(Location.Dashboard, "/dashboard"),
@@ -41,10 +48,16 @@ namespace AdaptiveAds_TestFramework
 
         #region Properties
 
+        /// <summary>
+        /// Browser automation object.
+        /// </summary>
         public static IWebDriver Instance { get; set; }
+        
+        private static Collection<Route> Routes { get; set; } 
 
-        public static Collection<Route> NavigableLocations { get; set; } 
-
+        /// <summary>
+        /// Duration automation famework must wait before erroring.
+        /// </summary>
         public static Period WaitPeriod
         {
             get { return _waitPeriod; }
@@ -61,6 +74,9 @@ namespace AdaptiveAds_TestFramework
 
         #region setup and teardown
 
+        /// <summary>
+        /// Sets up a new automation object.
+        /// </summary>
         public static void Initialise()
         {
             Close();
@@ -68,6 +84,9 @@ namespace AdaptiveAds_TestFramework
             WaitPeriod = Period.None;
         }
 
+        /// <summary>
+        /// Disposes of the automation object.
+        /// </summary>
         public static void Close()
         {
             if(Instance!=null)
@@ -78,90 +97,105 @@ namespace AdaptiveAds_TestFramework
 
         #region NavigableLocations
         
-        /// <summary>
-        /// TODO: Fill this in
-        /// </summary>
+       /// <summary>
+       /// Navigate browser to a given location.
+       /// </summary>
+       /// <param name="location">Location to navigate to.</param>
         public static void GoTo(Location location)
         {
-            foreach (Route route in NavigableLocations)
+            // Search for location in Routes.
+            foreach (Route route in Routes)
             {
                 if (route.Location == location)
                 {
+                    // Navigate browser to the location.
                     Driver.Instance.Navigate().GoToUrl(_address + route.UrlExtension);
-                    //Driver navigated to the specified location.
                     return;
                 }
             }
-            //Did not find the location in the list.
+            // Did not find the location in the Routes.
             throw new NotFoundException("Route not found.",
-                new NotImplementedException("Route not yet supported within Driver."));
+                new NotImplementedException("Location not yet supported within Driver."));
         }
 
         /// <summary>
         /// Ensures the Driver is at the specified location. 
         /// </summary>
-        /// <param name="location">Route to check the browser is at.</param>
+        /// <param name="location">Location to check the browser is at.</param>
         public static void IsAt(Location location)
         {
-            foreach (Route route in NavigableLocations)
+            // Search for location in Routes.
+            foreach (Route route in Routes)
             {
                 if (route.Location == location)
                 {
+                    // Check the browser is at the correct location.
                     if (Instance.Url != _address + route.UrlExtension)
                     {
                         //Driver is not at the specified location.
                         throw new WebDriverException("Incorrect location.",
                             new InvalidElementStateException(
-                                "The location did not match the driver." +
+                                "The given location did not match the browser." +
                                 " Expected \"" + _address + route.UrlExtension + "\" Actual \"" + Instance.Url + "\""));
                     }
                     //Driver is at the specified location.
                     return;
                 }
             }
-            //Did not find the location in the list.
+            //Did not find the location in Routes.
             throw new NotFoundException("Route not found.",
-                new NotImplementedException("Route not yet supported within Driver."));
+                new NotImplementedException("Location not yet supported within Driver."));
         }
 
         /// <summary>
         /// Ensures the Driver is not at the specified location.  
         /// </summary>
-        /// <param name="LocationName">Route to check the browser is not at.</param>
-        public static void IsNotAt(Location LocationName)
+        /// <param name="Location">Location to check the browser is not at.</param>
+        public static void IsNotAt(Location Location)
         {
-            foreach (Route location in NavigableLocations)
+            // Search for location in Routes.
+            foreach (Route location in Routes)
             {
-                if (location.Location == LocationName)
+                if (location.Location == Location)
                 {
+                    // Check the browser is not at the correct location.
                     if (Instance.Url == _address + location.UrlExtension)
                     {
                         //Driver is at the specified location.
                         throw new WebDriverException("Incorrect location.",
                             new InvalidElementStateException(
-                                "The location matched the driver."));
+                                "The given location matched the browser."));
                     }
                     //Driver is not at the specified location.
                     return;
                 }
             }
-            //Did not find the location in the list.
+            //Did not find the location in Routes.
             throw new NotFoundException("Route not found.",
-                new NotImplementedException("Route not yet supported within Driver."));
+                new NotImplementedException("Location not yet supported within Driver."));
         }
 
         #endregion//NavigableLocations
 
         #region WaitHandling
 
+        /// <summary>
+        /// Performs an action with a temporary wait period.
+        /// </summary>
+        /// <param name="waitPeriod">Period to wait while executing action.</param>
+        /// <param name="action">Action to execute.</param>
         public static void ActionWait(Period waitPeriod, Action action)
         {
-            Period wp = WaitPeriod;
+            Period previousPeriod = WaitPeriod;
             SetWait(waitPeriod);
             action();
-            SetWait(wp);
+            SetWait(previousPeriod);
         }
-
+        
+        /// <summary>
+        /// Updates the Automation instance wait period.
+        /// </summary>
+        /// <param name="wp">New wait period.</param>
         private static void SetWait(Period wp)
         {
             int miliseconds;
