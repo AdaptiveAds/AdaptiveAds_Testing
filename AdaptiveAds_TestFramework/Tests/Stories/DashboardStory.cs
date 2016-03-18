ee33ca77b0036e953beb9b1e8b36cb6d437dc535
liamharries <liamharries@googlemail.com>
2016-03-18 19:30:40 +0000

    Updated Dashboard tests to cover all links

--- AdaptiveAds_TestFramework/Tests/Stories/DashboardStory.cs
+++ AdaptiveAds_TestFramework/Tests/Stories/DashboardStory.cs
@@ -13,7 +13,7 @@ namespace Tests.Stories
     public class DashboardStory
     {
         #region Initialise and clean up
-        
+
         [OneTimeSetUp]
         public void Init()
         {
@@ -52,6 +52,76 @@ namespace Tests.Stories
                 .When(x => DashboardPage.Select(DashboardLink.Playlists), "When I select the playlists link.")
                 .Then(x => Driver.IsNotAt(Location.Dashboard), "Then I should no longer be at the Dashboard page.")
                 .And(x => Driver.IsAt(Location.Playlists), "I should be at the Playlists page.")
+                .BDDfy<DashboardStory>();
+        }
+
+        [Test]
+        public void UserCanNavigateToTheLocationsPageFromTheDashboard()
+        {
+            this.Given(x => Driver.IsAt(Location.Dashboard), "Given I am at the Dashboard.")
+                .When(x => DashboardPage.Select(DashboardLink.Locations), "When I select the locations link.")
+                .Then(x => Driver.IsNotAt(Location.Dashboard), "Then I should no longer be at the Dashboard page.")
+                .And(x => Driver.IsAt(Location.Locations), "I should be at the Locations page.")
+                .BDDfy<DashboardStory>();
+        }
+
+        [Test]
+        public void UserCanNavigateToTheDepartmentsPageFromTheDashboard()
+        {
+            this.Given(x => Driver.IsAt(Location.Dashboard), "Given I am at the Dashboard.")
+                .When(x => DashboardPage.Select(DashboardLink.Departments), "When I select the departments link.")
+                .Then(x => Driver.IsNotAt(Location.Dashboard), "Then I should no longer be at the Dashboard page.")
+                .And(x => Driver.IsAt(Location.Departments), "I should be at the Departments page.")
+                .BDDfy<DashboardStory>();
+        }
+
+        [Test]
+        public void UserCanNavigateToTheScreensPageFromTheDashboard()
+        {
+            this.Given(x => Driver.IsAt(Location.Dashboard), "Given I am at the Dashboard.")
+                .When(x => DashboardPage.Select(DashboardLink.Screens), "When I select the screens link.")
+                .Then(x => Driver.IsNotAt(Location.Dashboard), "Then I should no longer be at the Dashboard page.")
+                .And(x => Driver.IsAt(Location.Screens), "I should be at the Screens page.")
+                .BDDfy<DashboardStory>();
+        }
+
+        [Test]
+        public void UserCanNavigateToTheUsersPageFromTheDashboard()
+        {
+            this.Given(x => Driver.IsAt(Location.Dashboard), "Given I am at the Dashboard.")
+                .When(x => DashboardPage.Select(DashboardLink.Users), "When I select the users link.")
+                .Then(x => Driver.IsNotAt(Location.Dashboard), "Then I should no longer be at the Dashboard page.")
+                .And(x => Driver.IsAt(Location.Users), "I should be at the Users page.")
+                .BDDfy<DashboardStory>();
+        }
+
+        [Test]
+        public void UserCanNavigateToTheTemplatesPageFromTheDashboard()
+        {
+            this.Given(x => Driver.IsAt(Location.Dashboard), "Given I am at the Dashboard.")
+                .When(x => DashboardPage.Select(DashboardLink.Templates), "When I select the templates link.")
+                .Then(x => Driver.IsNotAt(Location.Dashboard), "Then I should no longer be at the Dashboard page.")
+                .And(x => Driver.IsAt(Location.Templates), "I should be at the Templates page.")
+                .BDDfy<DashboardStory>();
+        }
+
+        [Test]
+        public void UserCanNavigateToTheSkinsPageFromTheDashboard()
+        {
+            this.Given(x => Driver.IsAt(Location.Dashboard), "Given I am at the Dashboard.")
+                .When(x => DashboardPage.Select(DashboardLink.Skins), "When I select the skins link.")
+                .Then(x => Driver.IsNotAt(Location.Dashboard), "Then I should no longer be at the Dashboard page.")
+                .And(x => Driver.IsAt(Location.Skins), "I should be at the Skins page.")
+                .BDDfy<DashboardStory>();
+        }
+
+        [Test]
+        public void UserCanNavigateToThePrivilegesPageFromTheDashboard()
+        {
+            this.Given(x => Driver.IsAt(Location.Dashboard), "Given I am at the Dashboard.")
+                .When(x => DashboardPage.Select(DashboardLink.Privileges), "When I select the privileges link.")
+                .Then(x => Driver.IsNotAt(Location.Dashboard), "Then I should no longer be at the Dashboard page.")
+                .And(x => Driver.IsAt(Location.Privileges), "I should be at the Privileges page.")
                 .BDDfy<DashboardStory>();
         }
     }
